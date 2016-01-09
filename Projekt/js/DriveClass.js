ed92f8f724491f3c7df27f9aa9f52f11c1a812b8
WictorKihlbaum <wk222as@student.lnu.se>
2016-01-09 16:30:06 +0100

    Drive image editing update
    Full size Drive-images are now being fetched and can now be edited in
    original size.

--- Projekt/js/DriveClass.js
+++ Projekt/js/DriveClass.js
@@ -16,23 +16,30 @@ var DriveClass = {
 			theme: 'minimum',
 			tools: 'all',
 			appendTo: '',
+			displayImageSize: true, // remove later?.
 			onSave: function(imageID, newURL) {
 				var img = document.getElementById(imageID);
 				img.src = newURL;
+				img.style.width = "200px";
+				img.style.heigth = "225px";
 			},
+			
+			/*onClose: function(imageID, newURL) {
+				var img = document.getElementById(imageID);
+				img.src = newURL;
+				img.style.width = "200px";
+				img.style.heigth = "225px";
+			},*/
 			
 			onError: function(errorObj) {
 				alert(errorObj.message);
 			}
 		});
 		
-		console.log('featherEditor is instantiated');
 		console.log(DriveClass.featherEditor);
 	},
 	
 	launchEditor: function(id) {
-		console.log(id);
-		console.log(DriveClass.featherEditor);
 		
 		DriveClass.featherEditor.launch({
 			image: id
